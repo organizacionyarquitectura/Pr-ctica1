2c61f48ab563cc372be76b3863aea05ba9a6826b
quirec <quirec@localhost.localdomain>
2019-02-12 00:34:19 -0600

    Añadiendo normalización

--- script/analisis.py
+++ script/analisis.py
@@ -2,6 +2,18 @@ from scipy import stats as st
 import numpy as np
 import pandas as pd
 
+def mean(arr):
+    means = []
+    for v in arr :
+        means.append(np.mean(v))
+    return np.array(means)
+
+def hmean(arr):
+    means = []
+    for v in arr :
+        means.append(st.hmean(v))
+    return np.array(means)
+
 # Leer datos
 b_df = pd.read_excel('../datos/datos_benchmark.xlsx')
 
@@ -20,17 +32,23 @@ pr_data = np.c_[
 ]
 
 # Calculando medida de tiempo de respuesta (media armónica)
-arm_mean = []
-
-for v in rt_data :
-    arm_mean.append(st.hmean(v))
+arm_mean = hmean(rt_data)
 
 # Calculando medida de rendimiento (media aritmética)
-arit_mean = []
-
-for v in pr_data :
-    arit_mean.append(np.mean(v))
+arit_mean = mean(pr_data)
 
 #Imprimiendo resultados
 for i in range(len(arit_mean)):
     print(i+1, ': arit:', arit_mean[i], 'arm:', arm_mean[i])
+
+# Normalizando y recalculando medias
+n = arm_mean.sum()
+n_arm_mean = hmean(rt_data / n)
+
+n = arit_mean.sum()
+n_arit_mean = mean(pr_data / n)
+
+#Imprimiendo resultados
+print('Normalizado')
+for i in range(len(n_arit_mean)):
+    print(i+1, ': arit:', n_arit_mean[i], 'arm:', n_arm_mean[i])
